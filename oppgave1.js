8eec2657d38b427f58def4108c65fb09dca4e167
Fiery106 <fiery106@gmail.com>
2025-06-24 11:02:50 +0200

    promise

--- oppgave1.js
+++ oppgave1.js
@@ -2,12 +2,43 @@ let output = document.getElementById("output");
 
 
 function promiseFunc() {
+    reset()
 
+    fetch("land.json")
+        .then(response => response.json())
+        .then(data => {
+            data.countries.forEach(element => {
+                let div = document.createElement("div");
+                let h1 = document.createElement("h1");
+                let img = document.createElement("img");
+                let h2 = document.createElement("h2");
+                let p = document.createElement("p");
+
+                div.classList.add("card");
+                h1.classList.add("card-name");
+                img.classList.add("card-flag");
+                h2.classList.add("card-capital");
+                p.classList.add("card-inhabitants");
+
+                h1.textContent = element.name;
+                img.src = element.flag;
+                h2.textContent = element.capital;
+                p.textContent = element.inhabitants;
+
+                div.append(h1, img, h2, p)
+                output.innerHTML += div.outerHTML
+            });
+        })
 }
 
 
 function asyncFunc() {
-    
+    reset()
+
+
 }
 
-//output.innerhtml = 
+
+function reset() {
+    output.innerhtml = "";
+}
